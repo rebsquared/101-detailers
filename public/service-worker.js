eefdd470096b1c22b697a56a1237721ee461bbc1
rebsquared <rebsquared@gmail.com>
2026-09-20 19:45:50 -0700

    Add clean batch to results carousel

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "101-detailers-brand-v17";
+const CACHE = "101-detailers-brand-v18";
 const APP_SHELL = [
   "/",
   "/app.html",
@@ -44,7 +44,7 @@ const injectHomeEnhancements = async (response) => {
   if (!html.includes('/install-app.js')) {
     html = html.replace(
       '</body>',
-      '<script src="/install-app.js?v=17"></script>\n  </body>',
+      '<script src="/install-app.js?v=18"></script>\n  </body>',
     );
   }
 
